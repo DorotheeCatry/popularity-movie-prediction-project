6d581ef4a6d57c9d2a59fd081abb296cf55b4e95
Nickaos1203 <nicolas.guillon95@yahoo.com>
2025-04-08 15:42:14 +0200

    correctif

--- movies_scraping/movies_scraping/settings.py
+++ movies_scraping/movies_scraping/settings.py
@@ -1,92 +1,66 @@
-# Scrapy settings for movies_scraping project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+JOBDIR = 'jobdir'
 
 BOT_NAME = "movies_scraping"
-
 SPIDER_MODULES = ["movies_scraping.spiders"]
 NEWSPIDER_MODULE = "movies_scraping.spiders"
-
-
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "movies_scraping (+http://www.yourdomain.com)"
-
-# Obey robots.txt rules
+# Respecter les règles du robots.txt
 ROBOTSTXT_OBEY = True
-
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
-
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
-
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
+# Augmenter le nombre total de requêtes simultanées pour gagner en rapidité
+CONCURRENT_REQUESTS = 32
+# Limiter le nombre de requêtes par domaine et par IP pour éviter la surchage d'un même serveur
+CONCURRENT_REQUESTS_PER_DOMAIN = 8
+CONCURRENT_REQUESTS_PER_IP = 8
+# Réduire légèrement le délai entre les requêtes pour plus de rapidité, en laissant l'autothrottle ajuster au besoin
+DOWNLOAD_DELAY = 0.25
+# Désactiver les cookies si non nécessaires
+COOKIES_ENABLED = False
+# -----------------------------------------------------------------------------
+# Configuration des middlewares de téléchargement : rotation des User Agents
+# -----------------------------------------------------------------------------
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
+# Liste de User Agents variés pour la rotation automatique
+USER_AGENT_LIST = [
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edge/92.0.902.78',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edge/91.0.864.59',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (Linux; Android 10; Pixel 3 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; AS; .NET4.0E; .NET4.0C; en-US) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0',
+    'Mozilla/5.0 (Linux; Android 9; Galaxy S9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:40.0) Gecko/20100101 Firefox/40.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0',
+]
+# -----------------------------------------------------------------------------
+# Configuration de pipelines : ici, nous utilisons un pipeline CSV personnalisé
+# -----------------------------------------------------------------------------
+# ITEM_PIPELINES = {
 #}
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "movies_scraping.middlewares.MoviesScrapingSpiderMiddleware": 543,
-#}
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "movies_scraping.middlewares.MoviesScrapingDownloaderMiddleware": 543,
-#}
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "movies_scraping.pipelines.MoviesScrapingPipeline": 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
-
-# Set settings whose default value is deprecated to a future-proof value
+# -----------------------------------------------------------------------------
+# AutoThrottle permet d'adapter dynamiquement le rythme des requêtes pour éviter le blocage
+# -----------------------------------------------------------------------------
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 0.5      # Délai initial réduit pour démarrer rapidement
+AUTOTHROTTLE_MAX_DELAY = 5          # Délai maximal en cas de latence
+AUTOTHROTTLE_TARGET_CONCURRENCY = 4.0  # Viser 4 requêtes simultanées par serveur
+# -----------------------------------------------------------------------------
+# Paramètres relatifs au threadpool (augmenter légèrement si nécessaire)
+# -----------------------------------------------------------------------------
+REACTOR_THREADPOOL_MAXSIZE = 20
+# -----------------------------------------------------------------------------
+# Pour garantir la compatibilité avec les versions futures de Scrapy,
+# utilisez le réacteur asynchrone AsyncioSelectorReactor.
+# -----------------------------------------------------------------------------
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-FEED_EXPORT_ENCODING = "utf-8"
--- movies_scraping/movies_scraping/spiders/allocine_spider.py
+++ movies_scraping/movies_scraping/spiders/allocine_spider.py
@@ -9,7 +9,7 @@ class AllocineSpider(scrapy.Spider):
 
     allowed_domains = ["allocine.fr"]
 
-    start_urls = ["https://www.allocine.fr/films/decennie-2010/?page=" + str(x) for x in range(1, 3)]
+    start_urls = ["https://www.allocine.fr/films/decennie-2010/?page=" + str(x) for x in range(1, 1760)]
 
 
     def parse(self, response):
@@ -23,14 +23,19 @@ class AllocineSpider(scrapy.Spider):
 
     def parse_movie(self, response):
         title = response.css('div.titlebar-title::text').get()
+        title_clean = str(title)
 
         image = response.css('div.entity-card-player-ovw figure.thumbnail span.thumbnail-container img').attrib["src"]
+        image_clean = str(image)
 
         date = response.css('div.entity-card-player-ovw div.meta div.meta-body div.meta-body-item span.date::text').get().replace('\n', ' ').strip()
+        date_clean = str(date)
 
         broadcast_place = response.css('div.entity-card-player-ovw div.meta div.meta-body div.meta-body-info span.meta-release-type::text').get().replace('\n', ' ').strip()
+        broadcast_place_clean = str(broadcast_place)
 
         genres = response.css('div.entity-card-player-ovw div.meta div.meta-body div.meta-body-info span.dark-grey-link::text').getall()
+        genres_clean = [str(genre) for genre in genres]
 
         certificate = response.css("section.ovw-synopsis div.certificate span.certificate-text::text").get()
         certificate_clean = str(certificate.replace('\n', ' ').strip()) if certificate != None else "Tout public"
@@ -54,44 +59,98 @@ class AllocineSpider(scrapy.Spider):
         actors_clean = [str(actor) for actor in actors]
 
         average_notes_press = response.css("div.entity-card-player-ovw div.rating-holder div.rating-item div.rating-item-content div.stareval-theme-default span.stareval-note::text").get()
+
         average_notes_spectators = response.css("div.entity-card-player-ovw div.rating-holder div.rating-item div.rating-item-content div.stareval-theme-default span.stareval-note::text")[1].get()
 
-        nationality = response.css("section.ovw-technical div.item span.that span.nationality::text").getall()
-        nationality_clean = [str(country.strip()) for country in nationality]
+        # filtrage du nombre d'items 
+        technical_infos = response.css("section.ovw-technical div.item").getall()
 
-        distributor = response.css("section.ovw-technical div.item span.blue-link::text").get().strip()
+        if len(technical_infos) == 16:
+            nationality = response.css("section.ovw-technical div.item span.that span.nationality::text").getall()
+            nationality_clean = [str(country.strip()) for country in nationality]
 
-        awards = response.css("section.ovw-technical div.item span.blue-link::text")[1].get().replace('\n', ' ').strip()
-        awards_clean = str(awards)
+            distributor = response.css("section.ovw-technical div.item span.blue-link::text").get()
+            distributor_clean = str(distributor).strip()
 
-        year_production = response.css("section.ovw-technical div.item span.that::text")[4].get()
+            awards_clean = "Aucun"
 
-        movie_type = response.css("section.ovw-technical div.item span.that::text")[8].get().strip()
-        language = response.css("section.ovw-technical div.item span.that::text")[12].get().replace('\n', ' ').strip()
+            year_production = response.css("section.ovw-technical div.item span.that::text")[4].get()
+            year_production_clean = int(year_production)
 
-        # target
-        entrance_number_fr = response.css("section.ovw-technical div.item span.blue-link::text")[2].get().replace('\n', ' ').strip()
+            movie_type = response.css("section.ovw-technical div.item span.that::text")[8].get()
+            movie_type_clean = str(movie_type).strip()
+
+            languages = response.css("section.ovw-technical div.item span.that::text")[12].getall()
+            languages_clean = [str(language.replace('\n', '')).strip() for language in languages] if len(languages) > 1 else str(languages[0].replace('\n', '')).strip()
+
+            entrance_number_fr = response.css("section.ovw-technical div.item span.that::text")[10].get()
+            entrance_number_fr_clean = str(entrance_number_fr).replace('\n', ' ').strip()
+
+        elif len(technical_infos) == 18:
+            nationality = response.css("section.ovw-technical div.item span.that span.nationality::text").getall()
+            nationality_clean = [str(country.strip()) for country in nationality]
+
+            distributor = response.css("section.ovw-technical div.item span.blue-link::text").get()
+            distributor_clean = str(distributor).strip()
+
+            awards = response.css("section.ovw-technical div.item span.blue-link::text")[1].get()
+            awards_clean = str(awards.replace('\n', ' ').strip()) if awards != None else "None"
+
+            year_production = response.css("section.ovw-technical div.item span.that::text")[4].get()
+            year_production_clean = int(year_production)
+
+            movie_type = response.css("section.ovw-technical div.item span.that::text")[8].get()
+            movie_type_clean = str(movie_type).strip()
+
+            languages = response.css("section.ovw-technical div.item span.that::text")[13].getall()
+            languages_clean = [str(language.replace('\n', '')).strip() for language in languages] if len(languages) > 1 else str(languages[0].replace('\n', '')).strip()
+
+            entrance_number_fr = response.css("section.ovw-technical div.item span.blue-link::text")[2].get()
+            entrance_number_fr_clean = str(entrance_number_fr).replace('\n', ' ').strip()
+
+        else:
+            nationality = response.css("section.ovw-technical div.item span.that span.nationality::text").getall()
+            nationality_clean = [str(country.strip()) for country in nationality]
+
+            distributor = response.css("section.ovw-technical div.item span.blue-link::text").get()
+            distributor_clean = str(distributor).strip()
+
+            awards = response.css("section.ovw-technical div.item span.blue-link::text")[1].get()
+            awards_clean = str(awards.replace('\n', ' ').strip()) if awards != None else "None"
+
+            year_production = response.css("section.ovw-technical div.item span.that::text")[4].get()
+            year_production_clean = int(year_production)
+
+            movie_type = response.css("section.ovw-technical div.item span.that::text")[8].get()
+            movie_type_clean = str(movie_type).strip()
+
+            languages = response.css("section.ovw-technical div.item span.that::text")[12].getall()
+            languages_clean = [str(language.replace('\n', '')).strip() for language in languages] if len(languages) > 1 else str(languages[0].replace('\n', '')).strip()
+
+            # target
+            entrance_number_fr = response.css("section.ovw-technical div.item span.blue-link::text")[2].get()
+            entrance_number_fr_clean = str(entrance_number_fr).replace('\n', ' ').strip()
 
         yield {
-            'title': str(title),
-            'image': str(image),
-            'date': str(date),
-            'broadcast_place': str(broadcast_place),
-            'genres': genres,
-            'certificate': str(certificate_clean),
+            'title': title_clean,
+            'image': image_clean,
+            'date': date_clean,
+            'broadcast_place': broadcast_place_clean,
+            'genres': genres_clean,
+            'certificate': certificate_clean,
             'synopsis': synopsis_clean,
             'directors': directors_clean,
-            'scenarists': scenarists,
-            'actors': actors,
-            'average_notes_press': float(average_notes_press),
-            'average_notes_spectators': float(average_notes_spectators),
+            'scenarists': scenarists_clean,
+            'actors': actors_clean,
+            'average_notes_press': average_notes_press,
+            'average_notes_spectators': average_notes_spectators,
             'nationality': nationality_clean,
-            'distributor': str(distributor),
+            'distributor': distributor_clean,
             'awards': awards_clean,
-            'year_production': int(year_production),
-            'movie_type': str(movie_type),
-            'language': str(language),
-            'entrance_number_fr': str(entrance_number_fr),
+            'year_production': year_production_clean,
+            'movie_type': movie_type_clean,
+            'language': languages_clean,
+            'entrance_number_fr': entrance_number_fr_clean,
         }
 
 
