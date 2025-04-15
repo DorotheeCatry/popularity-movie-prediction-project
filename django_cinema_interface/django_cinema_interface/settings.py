8389e2ba1c1b8bc744d629a948ab0ae9b21b4002
Eliandy DUMORTIER <rymer.eliandy@gmail.com>
2025-04-15 14:36:30 +0200

    Integrate .env variables in settings.py

--- django_cinema_interface/django_cinema_interface/settings.py
+++ django_cinema_interface/django_cinema_interface/settings.py
@@ -11,19 +11,25 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import sys
+import dotenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#load env variables
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52*mx1unxf4h5w7c4^=05d*_-t+f3(m)j5cy!p5z@-&ua6e&#l'
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -75,7 +81,7 @@ WSGI_APPLICATION = 'django_cinema_interface.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, os.getenv('DATABASE_NAME', 'db.sqlite3')),
     }
 }
 
