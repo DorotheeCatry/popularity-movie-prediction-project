29bc98eaf5a1a7a86b28cd9776048e194172ed1c
Eliandy DUMORTIER <rymer.eliandy@gmail.com>
2025-04-23 12:02:11 +0200

    feat(movie_prediction): add templates and dynamic home dashboard
    - Add HTML templates for movies, programs, and entries (list and form views)
    - Update  to pull in and display dynamic data from  (next week’s program, latest entries, etc.)

--- django_cinema_interface/movie_prediction/admin.py
+++ django_cinema_interface/movie_prediction/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import (
+    Director, Actor, Genre, Movie,
+    Room, WeeklyProgram, DailyEntry
+)
 
-# Register your models here.
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'average_grade', 'popularity')
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'average_grade', 'popularity')
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('type',)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'release_date')
+    list_filter = ('year', 'genres')
+    search_fields = ('title',)
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'capacity')
+
+@admin.register(WeeklyProgram)
+class WeeklyProgramAdmin(admin.ModelAdmin):
+    list_display = ('week_start', 'room', 'movie')
+    list_filter = ('week_start', 'room')
+    autocomplete_fields = ('movie',)
+
+@admin.register(DailyEntry)
+class DailyEntryAdmin(admin.ModelAdmin):
+    list_display = ('date', 'room', 'entrances')
+    list_filter = ('date', 'room')
