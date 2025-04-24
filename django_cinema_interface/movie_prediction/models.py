c4209033f959fa1878d32336394d93b750f21bc9
Nickaos1203 <nicolas.guillon95@yahoo.com>
2025-04-24 16:44:44 +0200

    pre-update movie_prediction models

--- django_cinema_interface/movie_prediction/models.py
+++ django_cinema_interface/movie_prediction/models.py
@@ -1,106 +1,148 @@
 from django.db import models
 
-class Director(models.Model):
-    name = models.CharField(max_length=255)
-    average_grade = models.FloatField(null=True, blank=True)
-    popularity = models.FloatField(null=True, blank=True)
+
+class Stats_director(models.Model):
+    director = models.CharField(max_length=255)
+    director_average_entries = models.FloatField(null=True, blank=True)
+    director_med_entries = models.FloatField(null=True, blank=True)
+    director_count = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.director
 
-class Actor(models.Model):
-    name = models.CharField(max_length=255)
-    average_grade = models.FloatField(null=True, blank=True)
-    popularity = models.FloatField(null=True, blank=True)
 
-    def __str__(self):
-        return self.name
-
-class Genre(models.Model):
-    type = models.CharField(max_length=100)
+class Stats_actor(models.Model):
+    actor = models.CharField(max_length=255)
+    actor_average_entries = models.FloatField(null=True, blank=True)
+    actor_med_entries = models.FloatField(null=True, blank=True)
+    actor_count = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
-        return self.type
+        return self.actor
+
+
+class Stats_writer(models.Model):
+    writer = models.CharField(max_length=100)
+    writer_avg_entries = models.FloatField(null=True, blank=True)
+    writer_med_entries = models.FloatField(null=True, blank=True)
+    writer_count = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.writer
+
+
+class Stats_writer(models.Model):
+    writer = models.CharField(max_length=100)
+    writer_avg_entries = models.FloatField(null=True, blank=True)
+    writer_med_entries = models.FloatField(null=True, blank=True)
+    writer_count = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.writer
+
+
+class Stats_distributor(models.Model):
+    writer = models.CharField(max_length=100)
+    writer_avg_entries = models.FloatField(null=True, blank=True)
+    writer_med_entries = models.FloatField(null=True, blank=True)
+    writer_count = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.writer
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
+    release_date = models.DateField()
     duration = models.PositiveIntegerField(help_text="Duration in minutes")
+    genre = models.CharField(max_lengtth=255)
+    
+    # classification labels
+    audience = models.CharField(max_length=50, blank=True)
+
+    # nationalities of the movie
+    nationality = models.CharField(max_length=50, blank=True)
+
+    languages = models.CharField(max_length=100, blank=True)
     synopsis = models.TextField(blank=True)
     year = models.PositiveIntegerField()
-    country_production = models.CharField(max_length=100)
-    poster = models.URLField(blank=True)
-    release_date = models.DateField()
-    distributor = models.CharField(max_length=255, blank=True)
+    image_url = models.TextField(max_length=500, blank=True)
+    box_office_fr = models.models.PositiveIntegerField(null=True, blank=True)
 
-    # aggregated metrics
-    average_fr_director = models.FloatField(null=True, blank=True)
-    average_individual_actor = models.FloatField(null=True, blank=True)
-    total_average_actors = models.FloatField(null=True, blank=True)
-    sum_actors = models.FloatField(null=True, blank=True)
-    sum_popularity_actors = models.FloatField(null=True, blank=True)
 
-    # classification labels
-    audience_classification = models.CharField(max_length=50, blank=True)
-    languages = models.CharField(max_length=100, blank=True)
+#     country_production = models.CharField(max_length=100)
+#     poster = models.URLField(blank=True)
+    
+#     distributor = models.CharField(max_length=255, blank=True)
 
-    # target
-    number_entrances_fr = models.IntegerField(null=True, blank=True)
+#     # aggregated metrics
+#     average_fr_director = models.FloatField(null=True, blank=True)
+#     average_individual_actor = models.FloatField(null=True, blank=True)
+#     total_average_actors = models.FloatField(null=True, blank=True)
+#     sum_actors = models.FloatField(null=True, blank=True)
+#     sum_popularity_actors = models.FloatField(null=True, blank=True)
 
-    directors = models.ManyToManyField(Director, through='DirectorInMovie')
-    actors = models.ManyToManyField(Actor, through='ActorInMovie')
-    genres = models.ManyToManyField(Genre, through='GenreInMovie')
 
-    def __str__(self):
-        return self.title
+    
 
-class DirectorInMovie(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+#     # target
+#     number_entrances_fr = models.IntegerField(null=True, blank=True)
 
-    class Meta:
-        unique_together = ('movie', 'director')
+#     directors = models.ManyToManyField(Director, through='DirectorInMovie')
+#     actors = models.ManyToManyField(Actor, through='ActorInMovie')
+#     genres = models.ManyToManyField(Genre, through='GenreInMovie')
 
-class ActorInMovie(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+#     def __str__(self):
+#         return self.title
 
-    class Meta:
-        unique_together = ('movie', 'actor')
+# class DirectorInMovie(models.Model):
+#     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+#     director = models.ForeignKey(Director, on_delete=models.CASCADE)
 
-class GenreInMovie(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+#     class Meta:
+#         unique_together = ('movie', 'director')
 
-    class Meta:
-        unique_together = ('movie', 'genre')
+# class ActorInMovie(models.Model):
+#     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+#     actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
 
-class Room(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    capacity = models.PositiveIntegerField()
+#     class Meta:
+#         unique_together = ('movie', 'actor')
 
-    def __str__(self):
-        return self.name
+# class GenreInMovie(models.Model):
+#     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+#     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
-class WeeklyProgram(models.Model):
-    week_start = models.DateField()
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+#     class Meta:
+#         unique_together = ('movie', 'genre')
 
-    class Meta:
-        unique_together = ('week_start', 'room')
-        ordering = ['-week_start', 'room']
+# class Room(models.Model):
+#     name = models.CharField(max_length=50, unique=True)
+#     capacity = models.PositiveIntegerField()
 
-    def __str__(self):
-        return f"{self.week_start} - {self.room.name}: {self.movie.title}"
+#     def __str__(self):
+#         return self.name
 
-class DailyEntry(models.Model):
-    date = models.DateField()
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    entrances = models.PositiveIntegerField()
+# class WeeklyProgram(models.Model):
+#     week_start = models.DateField()
+#     room = models.ForeignKey(Room, on_delete=models.CASCADE)
+#     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
-    class Meta:
-        unique_together = ('date', 'room')
-        ordering = ['-date', 'room']
+#     class Meta:
+#         unique_together = ('week_start', 'room')
+#         ordering = ['-week_start', 'room']
 
-    def __str__(self):
-        return f"{self.date} - {self.room.name}: {self.entrances} entrées"
+#     def __str__(self):
+#         return f"{self.week_start} - {self.room.name}: {self.movie.title}"
+
+# class DailyEntry(models.Model):
+#     date = models.DateField()
+#     room = models.ForeignKey(Room, on_delete=models.CASCADE)
+#     entrances = models.PositiveIntegerField()
+
+#     class Meta:
+#         unique_together = ('date', 'room')
+#         ordering = ['-date', 'room']
+
+#     def __str__(self):
+#         return f"{self.date} - {self.room.name}: {self.entrances} entrées"
