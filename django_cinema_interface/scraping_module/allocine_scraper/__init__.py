babf7c085d64f65a6c95cac0da306322b0b5f88b
Dorothée Catry <114245234+DorotheeCatry@users.noreply.github.com>
2025-05-16 11:51:55 +0200

    Fix scraping module structure

--- django_cinema_interface/scraping_module/allocine_scraper/__init__.py
+++ django_cinema_interface/scraping_module/allocine_scraper/__init__.py
@@ -1,3 +1,6 @@
-from .settings import *
-from .pipelines import *
+# This makes the allocine_scraper a proper Python package
+from .items import *
 from .middlewares import *
+from .pipelines import *
+from .settings import *
+from .utils import *
