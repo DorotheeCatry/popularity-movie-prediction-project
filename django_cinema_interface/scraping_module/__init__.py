2cad930d340126329df1938538c232883b0ac029
Dorothée Catry <114245234+DorotheeCatry@users.noreply.github.com>
2025-05-16 12:03:41 +0200

    Fix scraping module structure and dependencies

--- django_cinema_interface/scraping_module/__init__.py
+++ django_cinema_interface/scraping_module/__init__.py
@@ -1,4 +1,4 @@
 # This makes the scraping module a proper Python package
-from .allocine_scraper import NewReleaseMovieSpider
+from .allocine_scraper.spiders.newreleases_spider import NewReleaseMovieSpider
 
 __all__ = ['NewReleaseMovieSpider']
