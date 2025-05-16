2c41fb78b5c7accf03d20dd7bb211fa66caa1495
Dorothée Catry <114245234+DorotheeCatry@users.noreply.github.com>
2025-05-16 11:55:25 +0200

    Fix scraping module structure

--- django_cinema_interface/scraping_module/allocine_scraper/__init__.py
+++ django_cinema_interface/scraping_module/allocine_scraper/__init__.py
@@ -1,24 +1,4 @@
 # This makes the allocine_scraper a proper Python package
-from .items import AllocineScraperParsingItem, MovieReleaseScraperParsingItem, MovieSuccessScraperItem
-from .middlewares import AllocineScraperSpiderMiddleware, AllocineScraperDownloaderMiddleware
-from .pipelines import ReleaseDatabasePipeline
-from .settings import *
-from .utils import parse_date, convert_to_minutes, clean_view_count, parse_numeric, safe_int_extraction, parse_brace_string, clean_pg_array_field
-from .spiders import NewReleaseMovieSpider
+from .spiders.newreleases_spider import NewReleaseMovieSpider
 
-__all__ = [
-    'AllocineScraperParsingItem',
-    'MovieReleaseScraperParsingItem',
-    'MovieSuccessScraperItem',
-    'AllocineScraperSpiderMiddleware',
-    'AllocineScraperDownloaderMiddleware',
-    'ReleaseDatabasePipeline',
-    'NewReleaseMovieSpider',
-    'parse_date',
-    'convert_to_minutes',
-    'clean_view_count',
-    'parse_numeric',
-    'safe_int_extraction',
-    'parse_brace_string',
-    'clean_pg_array_field'
-]
+__all__ = ['NewReleaseMovieSpider']
